fd47c328cec6249af6567badd1aeae706264de74
YauheniLikhachou <Yauheni_Likhachou2@epam.com>
2016-09-15 21:59:16 +0300

    task8

---
+++ task_8/cookbooks/jboss/metadata.rb
@@ -0,0 +1,8 @@
+name 'jboss'
+maintainer 'likhachou'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures jboss'
+long_description 'Installs/Configures jboss'
+version '0.1.0'
+depends 'java'
